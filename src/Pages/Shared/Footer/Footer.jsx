93035e6f5eeef68657d4df185a08ce174fc97b22
daiyan1998 <abduddaiyan7@gmail.com>
2023-06-07 03:10:58 +0600

    added footer

---
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,106 @@
+import { Typography } from "@material-tailwind/react";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { BsFacebook, BsGithub, BsTwitter } from "react-icons/bs";
+
+import logo from "../../../assets/shuriken.png";
+
+const Footer = () => {
+  const LINKS = [
+    {
+      title: "Product",
+      items: ["Overview", "Features", "Solutions", "Tutorials"],
+    },
+    {
+      title: "Company",
+      items: ["About us", "Careers", "Press", "News"],
+    },
+    {
+      title: "Resource",
+      items: ["Blog", "Newsletter", "Events", "Help center"],
+    },
+  ];
+
+  return (
+    <footer className="relative w-full mt-11">
+      <div className="mx-auto w-full max-w-7xl px-8">
+        <div className="grid grid-cols-1 justify-between gap-4 md:grid-cols-2">
+          <Typography
+            as="a"
+            href="#"
+            className="mr-4 cursor-pointer py-1.5 font-medium flex items-center gap-2"
+          >
+            <img className="h-12" src={logo} alt="" />
+            <span className="font-semibold">NinJutsu</span>
+          </Typography>
+          <div className="grid grid-cols-3 justify-between gap-4">
+            {LINKS.map(({ title, items }) => (
+              <ul key={title}>
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="mb-3 font-medium opacity-40"
+                >
+                  {title}
+                </Typography>
+                {items.map((link) => (
+                  <li key={link}>
+                    <Typography
+                      as="a"
+                      href="#"
+                      color="gray"
+                      className="py-1.5 font-normal transition-colors hover:text-blue-gray-900"
+                    >
+                      {link}
+                    </Typography>
+                  </li>
+                ))}
+              </ul>
+            ))}
+          </div>
+        </div>
+        <div className="mt-12 flex w-full flex-col items-center justify-center border-t border-blue-gray-50 py-4 md:flex-row md:justify-between">
+          <Typography
+            variant="small"
+            className="mb-4 text-center font-normal text-blue-gray-900 md:mb-0"
+          >
+            &copy; {"2023"}{" "}
+            <a href="https://material-tailwind.com/">Material Tailwind</a>. All
+            Rights Reserved.
+          </Typography>
+          <div className="flex gap-4 text-blue-gray-900 sm:justify-center">
+            <Typography
+              as="a"
+              href="#"
+              className="opacity-80 transition-opacity hover:opacity-100"
+            >
+              <BsFacebook></BsFacebook>
+            </Typography>
+            <Typography
+              as="a"
+              href="#"
+              className="opacity-80 transition-opacity hover:opacity-100"
+            >
+              <AiOutlineInstagram className="text-xl"></AiOutlineInstagram>
+            </Typography>
+            <Typography
+              as="a"
+              href="#"
+              className="opacity-80 transition-opacity hover:opacity-100"
+            >
+              <BsTwitter></BsTwitter>
+            </Typography>
+            <Typography
+              as="a"
+              href="#"
+              className="opacity-80 transition-opacity hover:opacity-100"
+            >
+              <BsGithub></BsGithub>
+            </Typography>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
