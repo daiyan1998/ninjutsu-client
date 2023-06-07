12503d082185e3a6785f4f8e6862767862da7254
daiyan1998 <abduddaiyan7@gmail.com>
2023-06-07 21:34:52 +0600

    added an extra section

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../Banner/Banner";
 import Classes from "../Classes/Classes";
+import Content from "../Content/Content";
 import Instructors from "../Instructors/Instructors";
 
 const Home = () => {
   return (
     <>
       <Banner></Banner>
+      <Content></Content>
       <Classes></Classes>
       <Instructors></Instructors>
     </>
